848f170987db97188a91fb64c2c520891492616f
lpgoulart <leonardo@roffor.com.br>
2018-06-14 00:09:38 -0300

    add Throwable

--- src/UserIsNotAuthenticatedException.java
+++ src/UserIsNotAuthenticatedException.java
@@ -1,6 +1,5 @@
-
 public class UserIsNotAuthenticatedException extends Exception{
-	public UserIsNotAuthenticatedException( String message ) {
-		super (message);
+	public UserIsNotAuthenticatedException( String message, Throwable e) {
+		super (message, e);
 	}
 }
